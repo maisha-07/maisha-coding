2aa3efa899d0abb06a733943638a3fac5c3cd882
Morshada Alam Maisha <maishahmed07@gmail.com>
2026-05-13 03:57:02 +0100

    Day 2: age calculator and simple calculator

---
+++ age.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+
+birth_year = int(input("what is you birth_year?"))
+current_year = 2026
+age = int(current_year - birth_year)
+print(f"Hello {name} you are {age} years old")
+
---
+++ calculator.py
@@ -0,0 +1,5 @@
+first_num = int(input("enter first number = "))
+
+sec_num = int(input("enter second number = "))
+total = int(first_num + sec_num)
+print(f"sum = {total}")
